e77e0f06f1dc5a208ac5a894e04d3af5602f83fc
cruz <cruzricardo23@gmail.com>
2026-01-29 02:37:10 +0000

    Add final3.py

---
+++ final3.py
@@ -0,0 +1,12 @@
+# Ricardo Cruz
+# Q3
+
+def NYE(times):
+    for i in range(times):
+        print("Happy New Year!")
+
+number = int(input("Input a number between 1 and 10: "))
+
+if 1 <= number <= 10:
+    NYE(number)
+
